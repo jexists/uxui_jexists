30784ad83aebcdff7ad976023b7182c2892e9318
jexists <jexists486@gmail.com>
2019-10-22 09:26:08 +0900

    coke on 191022

---
+++ w_portfolio_02_cocacola/js/src/jquery/burgur_btn.js
@@ -0,0 +1,17 @@
+// burgur_btn.js
+
+(function($){
+ const button = $('.burgur_btn');
+ const span = button.children('span');
+
+button.on('click',function(e){
+	e.preventDefault();
+ 	$(this).children(span).toggleClass('act');
+ });
+ button.on('focus',function(){
+ 	$(this).children(span).addClass('act');
+ });
+ button.on('blur',function(){
+ 	$(this).children(span).removeClass('act');
+ });
+})(jQuery);
