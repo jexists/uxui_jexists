b4b98e8becb74ff937f468a0cec11d7455adadbe
jexists <jexists486@gmail.com>
2019-10-29 15:26:25 +0900

    new example on 191029 (slide 2)

--- f_new_example/public/js/src/temp/slide_01.js
+++ f_new_example/public/js/src/temp/slide_01.js
@@ -10,18 +10,42 @@ const indicator = viewBox.find('.view_indicator');
 const indiLi = indicator.find('li');
 const vindiLink = indiLi.children('a');
 const guideBox = viewBox.find('.guide');
+const slideE = guideBox.children('div');
+let myn = 0;
+let maxn = slideE.length;
+let timed = 500;
 
 // indicator.css({'zIndex':500});
 // guideBox.css({'position':'relative', 'top':0,'left':0});
+const MoveSlide = function(n){
+	vindiLink.removeClass('action');
+	vindiLink.parent('li').eq(n).children('a').addClass('action');
+	guideBox.stop().animate({'left': -100 * n + '%'});
+}
+MoveSlide(0);
 
+
+let go;
+const stopSlide = function(){clearInterval(go);};
+const goSlide = function(){
+	go = setInterval(function(){
+	myn++; if (myn >= maxn) {myn = 0;}
+	MoveSlide(myn);},timed*6)
+};
+goSlide();
 vindiLink.on('click focus',function(e){
 	e.preventDefault();
+	// stopSlide();
 	let parLi = $(this).parent('li')
 	let i = parLi.index();
+	myn = i;
+	MoveSlide(myn);
 	// console.log(i);
-	vindiLink.removeClass('action');
+	/*vindiLink.removeClass('action');
 	$(this).addClass('action');
-	guideBox.stop().animate({'left': -100 * i + '%'});
+	guideBox.stop().animate({'left': -100 * i + '%'},timed);*/
 });
+  viewBox.on('mouseleave', stopSlide);
+  viewBox.on('mouseenter', goSlide);
 
 })(jQuery);
--- f_new_example/public/js/src/temp/slide_02_answer.js
+++ f_new_example/public/js/src/temp/slide_02_answer.js
@@ -8,16 +8,16 @@
 	const slideForm = viewBox2.find('.slide_02_form');
 	const slideGuide = slideForm.children('.guide');
 	const slideEach = slideGuide.children('.banner_area');
+	const viewBtn = viewBox2.find('.view_btn');
+	const playBtn = viewBtn.find('.play');
+	const pauseBtn = viewBtn.find('.pause');
 
 	let timed = 500; //0.5초
 	let myn = 0;
 	let maxn = slideEach.length; //슬라이드갯수
 
-	// slideEach.eq(0).addClass('action');
-	// // indiLiLink.eq(0).addClass('action');
-	// indiLi.eq(0).children(indiLiLink).addClass('action');
 // 공통기능 수행
-	const MoveSlide = function(n){
+	const MoveSlide = function(n, bool){
 	indiLiLink.removeClass('action');
 		indiLi.eq(n).children('a').addClass('action');
 		slideGuide.stop().animate({'marginLeft':(-100 * n) + '%'},
@@ -27,92 +27,68 @@
 					slideEach.eq(n).addClass('action');
 				},timed);
 				});
+		//play와 stop 버튼 동작 유무 판단
+		if(!bool){
+			playBtn.show();
+			pauseBtn.hide();
+		}else{
+			pauseBtn.show();
+			playBtn.hide();
+		}
 	};
-	MoveSlide(0);
+	MoveSlide(0, true);
 
-
-	//변수 i는 외부에서 공용으로 사용할 수 있도록, 전역 변수로 처리
-	//인디케이터, 광고 배너이동후 처리하는 부분 등의 내용은 별도 함수처리
-
-	//setInterval(), clearinterval()
 
 //일정시간마다 광고배너 움직이게하기
 
-	// let myn = 0; 지우기
 	let go; 
 	const StopSlide = function(){clearInterval(go);};
 	const GoSlide = function(){
    go = setInterval(function(){
 	 myn++; 
 	 if (myn >= maxn) {myn = 0;}
-	 MoveSlide(myn);
+	 MoveSlide(myn, true);
 	 },timed*6);};
 
-  GoSlide();
-  // viewBox2.on('mouseenter', StopSlide);
-  // viewBox2.on('mouseleave',	GoSlide);
+  // GoSlide(); 삭제
+  let playBanner = function(bool){
+  	if(bool){
+  		GoSlide();
+  	}else{
+			StopSlide();
+  	}
+  }//playBanner
+  playBanner(true);
 
-  viewBox2.on({'mouseenter':StopSlide, 'mouseleave':GoSlide});
+	pauseBtn.on('click',function(){playBanner(false);
+		/*StopSlide();
+		$(this).hide();
+		$(this).siblings().show();*/
+	});
+  playBtn.on('click',function(){playBanner(true);
+  	/*GoSlide();
+  	$(this).hide();
+		$(this).siblings().show();*/
+	});
+
+  viewBox2.on('mouseenter',function(){playBanner(false);});
+  viewBox2.on('mouseleave',function(){playBanner(true);});
 
 // 클릭시 배너 움직이게 만들기
+// playBtn.hide(); //지우기
   indiLiLink.on('click focus',function(e){
 		e.preventDefault();
+		e.stopPropagation(); //움직임이 부드럽게 제거 
+		// console.log(e.bubbles);
 		StopSlide(); //포커스일때 멈춰라
 		let myThis = $(this);
 		let myThisPar = myThis.parent('li');
 		let i = myThisPar.index();
 		myn = i; //let 사용X
-		MoveSlide(myn);
+		MoveSlide(myn, true);
+		playBanner(false);
 	});
 
-/*let i = 0;
-let maxi = 3;
-timed = 3000;
-let go; 
-let Goslide = function(){
-go = setInterval(function(){
-	i++; 
-	if(i >= maxi){ i = 0;}
 
-		indiLiLink.removeClass('action');
-		indiLiLink.eq(i).addClass('action');
-		slideGuide.animate({'marginLeft':(-100 * i) + '%'},
-			function(){
-				slideEach.removeClass('action');
-				setTimeout(function(){
-					slideEach.eq(i).addClass('action');
-				});
-				});
-},timed);};
-
-	Goslide(); //최초의 수행
-viewBox2.on('mouseenter',function(){
-	clearInterval(go); //강제로 삭제하는 기능
-});
-viewBox2.on('mouseleave',function(){
-	Goslide(); //벗어나면 다시 기능 넣기
-});*/
-
-//triger
-	
 })(jQuery);
 
-// let i = 0;
-// let maxi = 4;
-// let timed = 1000;
-// let go; 
-// let Goslide = function(){
-// go = setInterval(function(){
-// 	i++; 
-// 	if(i >= maxi){ i = 0;}
-// 		console.log(i);
-// },timed);};
-
-// 	Goslide(); //최초의 수행
-// $(document).on('mouseenter',function(){
-// 	clearInterval(go); //강제로 삭제하는 기능
-// });
-// $(document).on('mouseleave',function(){
-// 	Goslide(); //벗어나면 다시 기능 넣기
-// });
-// setTimeout(function(){clearInterval(go);},timed*15);
