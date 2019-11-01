050f6de728a7a413499a0bdde26db606243a7314
jexists <jexists486@gmail.com>
2019-11-01 11:01:58 +0900

    slide examples

--- f_new_example/public/js/src/temp/paul_viewBox.js
+++ f_new_example/public/js/src/temp/paul_viewBox.js
@@ -10,18 +10,24 @@
 	const slide = viewBox.find('.slide_form');
 	const slideUl = slide.find('ul');
 	const slideLi = slideUl.find('li');
-
-
+//시작
 	let liLen = slideLi.length;
+	const slideZindexSet = function(){ //반목문 함수화!
 	for(let i=0; i<liLen; i++){ //+반복문
 		slideLi.eq(i).css({zIndex:liLen-i});
 	}
+	}
+	slideZindexSet(); //함수화했던 반복문 실행
+//indicator 숫자 변환
 	let textN = 0; 
 	const textEm = function(n){indiCon.text("0" + (n+1));};
 	textEm(textN);
-
+//첫번째 슬라이드빼고 숨기기
+	slideLi.eq(textN).siblings().hide();
+//indicator를 누를시 기능
 	indiBtn.on('click', function(e){
 		e.preventDefault();
+		indiBtn.attr({'disable':'true'});
 		if ($(this).index() == 0 ){
 			textN++;
 			if(textN > liLen-1){textN = 0}
@@ -31,7 +37,18 @@
 			if(textN < 0){textN = liLen-1}
 			textEm(textN);
 		}
-
-	})
+		// slideLi.eq(textN).siblings().fadeOut();
+		// slideLi.eq(textN).fadeIn();
+		let slideThis = slideLi.eq(textN)
+		slideThis.css({zIndex: liLen+2, width: 0, overflow:'hidden'});
+		slideThis.stop().slideDown(function(){
+			slideThis.animate({width:100 + "%"},
+				1000,'easeOutCubic',function(){
+				slideThis.siblings().hide();
+				slideZindexSet();
+				indiBtn.attr({'disable':'false'});
+			}); // slideThis.animate
+		}); //slideThis.slideDown
+	}); //indiBtn.Click
 
 })(jQuery);
