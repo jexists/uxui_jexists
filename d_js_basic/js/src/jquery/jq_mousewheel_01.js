373e6bfe83e148d71904a403b2a4af2dc7677395
jexists <jexists486@gmail.com>
2019-11-12 18:19:24 +0900

    js scroll on 191112

--- d_js_basic/js/src/jquery/jq_mousewheel_01.js
+++ d_js_basic/js/src/jquery/jq_mousewheel_01.js
@@ -71,4 +71,50 @@
 	}
  });
 
+ //======================================
+ const gnb = $('#gnb');
+ const gnbUl = gnb.find('ul');
+ const gnbLi = gnb.find('li');
+ const gnbLink = gnbLi.find('a');
+
+ gnbLink.on('click',function(e){
+ 	e.preventDefault();
+ 	//let thisLink = $(this).attr('href');
+ 	useN = $(this).parent('li').index();
+ 	//let thisOffset = $(thisLink).offset().top;
+ 	//htmlEl.animate({scrollTop:thisOffset});
+ 	ScrollMagic();
+ });
+
+
+//터치패드 트랙패드 사용시
+let startP;
+let endP;
+
+$(window).on('touchstart',function(e){ //화면에 손가락이 닿는 순간
+	startP = e.originalEvent.touches[0].pageY;
+});
+$(window).on('touchmove',function(e){ //touchmove: 움직일때 
+		//ScrollMagic(); //배경을 움직이지 않게 멈추기 기능
+		htmlEl.animate({scrollTop:myScrollElTop[useN]},0)
+	});
+$(window).on('touchend',function(e){ // 화면에 손가락을 
+		//e.preventDefault();
+		endP = e.originalEvent.changedTouches[0].pageY;
+if(myStatus){
+		myStatus = false; // 트랙패드?오류 상태를 강제적으로 바꾸기 
+		if (startP>endP){
+			useN++;
+			if(useN >= scrollLen){useN=scrollLen-1;}
+			console.log(useN);
+			ScrollMagic();
+		}else{
+			useN--;
+			if(useN<=-1){useN=0;}
+			console.log(useN);
+			ScrollMagic();
+		}
+	}
+	});
+
 })(jQuery);
